87f50f0b95aa325db548b47665db1e5d4503d50a
Muhammad Luthfi Albiansyah <46187943+LLuthfiY@users.noreply.github.com>
2022-09-23 21:43:49 +0700

    main button and tailwindcss

---
+++ src/lib/main/partials/card.svelte
@@ -0,0 +1,23 @@
+<div
+	class=" w-[200px]  bg-slate-800 hover:bg-slate-700 transition-all rounded-lg flex flex-col cursor-pointer"
+>
+	<div class=" flex p-2">
+		<div class=" w-4 h-4 bg-red-400 mx-1 rounded-full" />
+		<div class=" w-4 h-4 bg-yellow-400 mx-1 rounded-full" />
+		<div class=" w-4 h-4 bg-green-400 mx-1 rounded-full" />
+	</div>
+	<div class=" bg-slate-600 m-1 grow rounded-lg p-2 h-[300px] flex relative ">
+		<img
+			src="icons/linux.svg"
+			alt="linux"
+			class="bg-linux mx-auto top-0  absolute opacity-40 w-11/12"
+		/>
+		<p class=" mt-auto text-blue-300 font-semibold">Operation System</p>
+	</div>
+</div>
+
+<style>
+	.bg-linux {
+		height: 100%;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Card from '$lib/main/partials/card.svelte';
+	import Github from '$lib/icons/github.svg';
+	import Mail from '$lib/icons/mail.svg';
+	import LinkedIn from '$lib/icons/linkedin.svg';
+	import SVG from 'svelte-inline-svg';
+</script>
+
+<div class=" bg-slate-900 flex flex-col min-h-screen w-screen">
+	<div class=" flex container mx-auto py-2 text-white text-xl">
+		<h1>Multi Styles CV</h1>
+		<div class=" ml-auto flex">
+			<a href="https://github.com/LLuthfiY" class=" my-auto">
+				<SVG src={Github} class="icon my-auto hover:text-orange-400 icon w-6 h-6 mx-2" />
+			</a>
+			<a href="mailto:mluthfia00@gmail.com" class=" my-auto">
+				<SVG src={Mail} class="icon my-auto hover:text-red-500 icon w-6 h-6 mx-2" />
+			</a>
+			<SVG src={LinkedIn} class="icon my-auto hover:text-blue-500 icon w-6 h-6 mx-2" />
+		</div>
+	</div>
+	<div class=" flex-grow flex justify-center items-center py-4  ">
+		<Card />
+	</div>
+</div>
+
+<style>
+</style>
